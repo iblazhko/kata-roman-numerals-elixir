a2ba1fed7d30d8d29a63ca7249c5d5b532e636e6
Ivan Blazhko <ivan.blazhko@gmail.com>
2018-12-01 14:31:13 +0000

    (Red) 1 converts to I

--- roman_numerals/test/roman_numerals_test.exs
+++ roman_numerals/test/roman_numerals_test.exs
@@ -5,4 +5,8 @@ defmodule RomanNumeralsTest do
   test "number 0 converts to an empty string" do
     assert RomanNumerals.romanize(0) == ""
   end
+
+  test "number 1 converts to I" do
+    assert RomanNumerals.romanize(1) == "I"
+  end
 end
