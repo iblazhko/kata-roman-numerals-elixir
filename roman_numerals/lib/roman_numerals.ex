6f3823384a7c937a93bbcb7bf59766bdd71c33f7
Ivan Blazhko <ivan.blazhko@gmail.com>
2018-12-02 22:43:54 +0000

    (Refactor) Using buckets-based translation

--- roman_numerals/lib/roman_numerals.ex
+++ roman_numerals/lib/roman_numerals.ex
@@ -3,11 +3,27 @@ defmodule RomanNumerals do
   Roman numerals.
   """
 
-  @numerals %{
+  @buckets %{
     0 => "",
     1 => "I",
-    2 => "II",
     5 => "V"}
+
+  defp find_largest_bucket(number) do
+    is_full? = fn v,_ -> v <= number end
+    full_buckets = :maps.filter(is_full?, @buckets)
+    [largest_bucket] = Enum.sort_by(full_buckets, &(-elem(&1,0)))
+    |> Enum.take(1)
+    largest_bucket
+  end
+
+  defp process_bucket(numerals, reminder) when reminder == 0 do
+    Enum.join(numerals)
+  end
+
+  defp process_bucket(numerals, reminder) do
+    {bucket_value, bucket_numerals} = find_largest_bucket(reminder)
+    process_bucket(numerals++[bucket_numerals], reminder-bucket_value)
+  end
 
   @doc """
   Converts number to its roman numerals representation.
@@ -21,6 +37,6 @@ defmodule RomanNumerals do
   //    MCMLIV
   """
   def romanize(number) do
-    @numerals[number]
+    process_bucket([], number)
   end
 end
