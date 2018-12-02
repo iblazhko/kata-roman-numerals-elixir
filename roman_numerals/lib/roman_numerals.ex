b1010e1668ae181848c3c1c33e3523aa584d2e7c
Ivan Blazhko <ivan.blazhko@gmail.com>
2018-12-02 23:11:13 +0000

    (Green) Complete test coverage for subtractive notation

--- roman_numerals/lib/roman_numerals.ex
+++ roman_numerals/lib/roman_numerals.ex
@@ -5,6 +5,7 @@ defmodule RomanNumerals do
 
   @buckets %{
     0 => "",
+
     1 => "I",
     5 => "V",
     10 => "X",
@@ -12,7 +13,13 @@ defmodule RomanNumerals do
     100 => "C",
     500 => "D",
     1000 => "M",
-    4 => "IV"
+
+    4 => "IV",
+    9 => "IX",
+    40 => "XL",
+    90 => "XC",
+    400 => "CD",
+    900 => "CM"
   }
 
   defp find_largest_bucket(number) do
