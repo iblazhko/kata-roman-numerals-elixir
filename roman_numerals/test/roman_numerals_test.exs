fe5cd436d03145d85fa4bfe73dd334e5baf3eadd
Ivan Blazhko <ivan.blazhko@gmail.com>
2018-12-02 23:04:32 +0000

    (Refactor) Same letter numerals

--- roman_numerals/test/roman_numerals_test.exs
+++ roman_numerals/test/roman_numerals_test.exs
@@ -34,8 +34,36 @@ defmodule RomanNumeralsTest do
     assert RomanNumerals.romanize(1000) == "M"
   end
 
-  test "number 2 converts to II" do
+  test "same letter numerals: number 2 converts to II" do
     assert RomanNumerals.romanize(2) == "II"
+  end
+
+  test "same letter numerals: number 3 converts to III" do
+    assert RomanNumerals.romanize(3) == "III"
+  end
+
+  test "same letter numerals: number 20 converts to XX" do
+    assert RomanNumerals.romanize(20) == "XX"
+  end
+
+  test "same letter numerals: number 30 converts to XXX" do
+    assert RomanNumerals.romanize(30) == "XXX"
+  end
+
+  test "same letter numerals: number 200 converts to CC" do
+    assert RomanNumerals.romanize(200) == "CC"
+  end
+
+  test "same letter numerals: number 300 converts to CCC" do
+    assert RomanNumerals.romanize(300) == "CCC"
+  end
+
+  test "same letter numerals: number 2000 converts to MM" do
+    assert RomanNumerals.romanize(2000) == "MM"
+  end
+
+  test "same letter numerals: number 3000 converts to MMM" do
+    assert RomanNumerals.romanize(3000) == "MMM"
   end
 
   test "number 4 converts to IV" do
