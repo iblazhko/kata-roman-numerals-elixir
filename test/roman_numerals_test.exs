2766b4ed935c07652ea0c472ffbe7c1ea95dc81d
Ivan Blazhko <ivan.blazhko@gmail.com>
2018-12-08 19:06:06 +0000

    (Refactor) Extracted assertion code

--- test/roman_numerals_test.exs
+++ test/roman_numerals_test.exs
@@ -2,26 +2,32 @@ defmodule RomanNumeralsTest do
   use ExUnit.Case
   doctest RomanNumerals
 
-  test "negative numbers cannot be converted to a roman numeral" do
-    test_cases = [
-      -1,
-      -2,
-      -100
-    ]
+  defp assert_out_of_range(test_cases) do
     for number <- test_cases do
       assert_raise ArgumentError, fn -> RomanNumerals.romanize(number) end
     end
   end
 
+  defp assert_romanized(test_cases) do
+    for {number, roman} <- test_cases do
+      assert RomanNumerals.romanize(number) == roman, "#{number} -> #{roman}"
+    end
+  end
+
+  test "negative numbers cannot be converted to a roman numeral" do
+    assert_out_of_range([
+      -1,
+      -2,
+      -100
+    ])
+  end
+
   test "numbers over 4000 cannot be converted to a roman numeral" do
-    test_cases = [
+    assert_out_of_range([
       4000,
       4001,
       5000
-    ]
-    for number <- test_cases do
-      assert_raise ArgumentError, fn -> RomanNumerals.romanize(number) end
-    end
+    ])
   end
 
   test "number 0 converts to an empty string" do
@@ -29,7 +35,7 @@ defmodule RomanNumeralsTest do
   end
 
   test "single letter numerals" do
-    test_cases = [
+    assert_romanized([
       {    1, "I" },
       {    5, "V" },
       {   10, "X" },
@@ -37,14 +43,11 @@ defmodule RomanNumeralsTest do
       {  100, "C" },
       {  500, "D" },
       { 1000, "M" }
-    ]
-    for {number, roman} <- test_cases do
-      assert RomanNumerals.romanize(number) == roman, "#{number} -> #{roman}"
-    end
+    ])
   end
 
   test "same letter numerals" do
-    test_cases = [
+    assert_romanized([
       {    2, "II"  },
       {    3, "III" },
       {   20, "XX"  },
@@ -53,28 +56,22 @@ defmodule RomanNumeralsTest do
       {  300, "CCC" },
       { 2000, "MM"  },
       { 3000, "MMM" }
-    ]
-    for {number, roman} <- test_cases do
-      assert RomanNumerals.romanize(number) == roman, "#{number} -> #{roman}"
-    end
+    ])
   end
 
   test "subtractive notation" do
-    test_cases = [
+    assert_romanized([
       {    4, "IV" },
       {    9, "IX" },
       {   40, "XL" },
       {   90, "XC" },
       {  400, "CD" },
       {  900, "CM" }
-    ]
-    for {number, roman} <- test_cases do
-      assert RomanNumerals.romanize(number) == roman, "#{number} -> #{roman}"
-    end
+    ])
   end
 
   test "mixed numbers should convert using additive pattern" do
-    test_cases = [
+    assert_romanized([
       {   39, "XXXIX"       },
       {  207, "CCVII"       },
       {  246, "CCXLVI"      },
@@ -83,9 +80,6 @@ defmodule RomanNumeralsTest do
       { 1873, "MDCCCLXXIII" },
       { 1984, "MCMLXXXIV"   },
       { 2018, "MMXVIII"     }
-    ]
-    for {number, roman} <- test_cases do
-      assert RomanNumerals.romanize(number) == roman, "#{number} -> #{roman}"
-    end
+    ])
   end
 end
